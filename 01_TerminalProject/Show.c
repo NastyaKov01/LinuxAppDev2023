bb9b6f7dfca216fd240c07c1fb091b3fd59cf52a
Anastasia Kovalenko <anastasija.kovalenko@tetrika.school>
2023-09-21 22:56:36 +0300

    Initial commit

---
+++ 01_TerminalProject/Show.c
@@ -0,0 +1,21 @@
+#include <curses.h>
+#include <stdio.h>
+
+
+int main(int argc, char* argv[])
+{
+    FILE* file;
+    char lines[10][100];
+    printf("ARGC: %d\n", argc);
+    printf("filename: %s\n", argv[1]);
+    file = fopen(argv[1], "r");
+    if (file == NULL) {
+        printf("Can not open the file\n");
+    }
+    while(fgets(line, 100, file) != NULL) {
+        printf("%s", line);
+    }
+    fclose(file);
+    return 0;
+}
+
